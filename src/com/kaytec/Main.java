5c61932f512c5ab14aeb2fc2ac568e308e80f15e
keenanhumm <keenan.humm@huskers.unl.edu>
2019-07-30 19:01:15 -0500

    rankings and comparing teams of same type

--- src/com/kaytec/Main.java
+++ src/com/kaytec/Main.java
@@ -1,6 +1,14 @@
 package com.kaytec;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    private static void printRankings(List<Team> teams) {
+        System.out.println("Rankings");
+        teams.forEach(Team::printRanking);
+    }
 
     public static void main(String[] args) {
         FootballPlayer joe = new FootballPlayer("Joe");
@@ -14,5 +22,33 @@ public class Main {
 
         Team<BaseballPlayer> baseballTeam = new Team<>("The Chicago Cubs");
         baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordon");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(crows, 3, 8);
+
+        crows.matchResult(fremantle, 2,1);
+//        crows.matchResult(baseballTeam, 1,1);
+
+        ArrayList<Team> teams = new ArrayList<>();
+        teams.add(hawthorn);
+        teams.add(baseballTeam);
+        teams.add(brokenTeam);
+        teams.add(melbourne);
+        teams.add(fremantle);
+        teams.add(crows);
+
+        printRankings(teams);
+
+
     }
 }
--- src/com/kaytec/Team.java
+++ src/com/kaytec/Team.java
@@ -2,7 +2,7 @@ package com.kaytec;
 
 import java.util.ArrayList;
 
-public class Team<T extends Player> {
+public class Team<T extends Player> implements Comparable<Team<T>>{
     private String name;
     private int played;
     private int won;
@@ -25,35 +25,55 @@ public class Team<T extends Player> {
 
     public boolean addPlayer(T player) {
         if (members.contains(player)) {
-            System.out.println(((Player)player).getName() + " is already on the team");
+            System.out.println(player.getName() + " is already on the team");
             return false;
         } else {
             members.add(player);
-            System.out.println(((Player)player).getName() + " was picked for " + this.name);
+            System.out.println(player.getName() + " was picked for " + this.name);
             return true;
         }
     }
 
     public int numPlayers() {
         return this.members.size();
-    }
+}
 
-    public void matchResult(Team opponent, int ourScore, int opponentScore) {
+    public void matchResult(Team<T> opponent, int ourScore, int opponentScore) {
+        String message;
+
         if (ourScore > opponentScore) {
+            message = " beat ";
             won++;
         } else if (opponentScore > ourScore) {
+            message = " lost to ";
             lost++;
         } else {
+            message = " tied with ";
             tied++;
         }
         played++;
 
         if (opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, opponentScore, ourScore);
         }
     }
 
     public int ranking() {
         return (won * 2) + tied;
+    }
+
+    public void printRanking() {
+        System.out.println(this.getName() + ": " + this.ranking());
+    }
+
+    @Override
+    public int compareTo(Team<T> team) {
+        if(this.ranking() > team.ranking()){
+            return -1;
+        } else if(this.ranking() < team.ranking()) {
+            return 1;
+        }
+        return 0;
     }
 }
